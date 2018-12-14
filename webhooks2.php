57494375de6b0ad32356bc5886dede04d5ff5c92
yod075 <45867900+yod075@users.noreply.github.com>
2018-12-14 15:59:57 +0700

    Update webhooks2.php

--- webhooks2.php
+++ webhooks2.php
@@ -52,8 +52,8 @@
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "2";
         $arrayPostData['messages'][1]['stickerId'] = "40";
-        $arrayPostData['messages'][2]['type'] = "text";
-        $arrayPostData['messages'][2]['text'] = "จะไปก็ไปเลย";
+        $arrayPostData['messages'][2]['type'] = "text1";
+        $arrayPostData['messages'][2]['text1'] = "จะไปก็ไปเลย";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Video"
