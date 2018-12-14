f8142a3001e5efafed02a4699142754423891483
yod075 <45867900+yod075@users.noreply.github.com>
2018-12-14 16:17:31 +0700

    Update webhooks2.php

--- webhooks2.php
+++ webhooks2.php
@@ -44,6 +44,16 @@
         $arrayPostData['messages'][0]['longitude'] = "100.532752";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    #ตัวอย่าง Message Type "Location"
+    else if($message == "พิกัดไฟฟ้า"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "location";
+        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+        $arrayPostData['messages'][0]['address'] =   "9.941590,99.080223";
+        $arrayPostData['messages'][0]['latitude'] = "9.941590";
+        $arrayPostData['messages'][0]['longitude'] = "99.080223";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
     else if($message == "ลาก่อน"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
