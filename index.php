cc5d74bb7db38a87944754ae8e53f70ca11b1db1
9ํYOD SOMBATNA <45867900+yod075@users.noreply.github.com>
2018-12-21 09:41:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 echo "Hello LINE BOT 9YOD";
+<h1>My First Heading</h1>;
+<h2>My First Heading</h2>;
+<h3>My First Heading</h3>;
+<h4>My First Heading</h4>;
+<h5>My First Heading</h5>;
+<h6>My First Heading</h6>;
+<p>My first paragraph.</p>;
+
